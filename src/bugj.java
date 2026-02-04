ce1e226c007dbada564c1955308e76e433457d57
vellaiyanvellaiyan432-ui <vellaiyanvellaiyan432@gmail.com>
2026-02-04 20:06:55 +0530

    Initial commit for this project

---
+++ src/bugj.java
@@ -0,0 +1,14 @@
+public class bugj {
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+
+
+    }
+    static int fibo(int n) {
+        if (n < 2) {
+
+            return n;
+        }
+        return  fibo(n-1)+fibo(n-2);
+    }
+}
