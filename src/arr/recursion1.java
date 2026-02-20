8e3fbd7898d078b51436023f6ef051fd0f33599c
vellaiyanvellaiyan432-ui <vellaiyanvellaiyan432@gmail.com>
2026-02-20 19:11:38 +0530

    Initial commit for this project

---
+++ src/arr/recursion1.java
@@ -0,0 +1,15 @@
+package arr;
+
+public class recursion1 {
+    public static void main(String[] args) {
+        int[]arr={4,7,9,10,15};
+        System.out.println(sorted(arr,0));
+
+    }
+    static boolean sorted(int[]arr,int index){
+        if (index== arr.length-1){
+            return true;
+        }
+        return arr[index]<arr[index+1]&& sorted(arr,index+1);
+    }
+}
