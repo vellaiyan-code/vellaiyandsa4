fdb16e7408ec954ecde4837f154f42b5f0a1c9a3
vellaiyandsa4 <vellaiyan09@gmail.com>
2026-02-24 20:16:50 +0530

    Initial commit for this project

--- src/bubblerec.java
+++ src/bubblerec.java
@@ -5,6 +5,8 @@ public class bubblerec {
         int []arr={4,3,2,1,};
         bubble(arr, arr.length-1,0);
         System.out.println(Arrays.toString(arr));
+        selection(arr, arr.length,0,0 );
+        System.out.println(Arrays.toString(arr));
 
     }
     static void bubble(int [] arr,int r,int c){
@@ -23,4 +25,23 @@ public class bubblerec {
             bubble(arr,r-1,0);
         }
     }
-}
+    static void selection(int []arr,int r,int c,int max){
+        if (r==0){
+            return;
+        }
+        if (c<r){
+            if (arr[c]>arr[max]){
+                selection(arr,r,c+1,c);
+            }else {
+                selection(arr,r,c+1,max);
+            }
+        }else {
+            int temp=arr[max];
+            arr[max]=arr[r-1];
+            arr[r-1]=temp;
+            selection(arr,r-1,0,0);
+        }
+        }
+
+    }
+
